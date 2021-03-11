bfaf44df6e389152a05fa350c1afde3364e3a99a
Emilia <emiartola@gmail.com>
2021-03-11 12:23:36 -0300

    creando repo desde carpeta 3

---
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/MicroserviciosUsuariosApplication.java
@@ -0,0 +1,15 @@
+package com.formacionbdi.microservicios.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class MicroserviciosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosUsuariosApplication.class, args);
+	}
+
+}
---
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoService.java
@@ -0,0 +1,12 @@
+package com.formacionbdi.microservicios.app.usuarios.services;
+
+import java.util.Optional;
+
+import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
+
+public interface AlumnoService {
+	public Iterable<Alumno> findAll();
+	public Optional<Alumno> findById(Long id);
+	public Alumno save(Alumno alumno);
+	public void deleteById(Long id);
+}
